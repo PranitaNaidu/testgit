b964f7c6677ea3b043e8b5a417a61e88842e8318
Pranita <pranita.naidu@yahoo.in>
2024-08-05 22:37:13 +0530

    First commit of maven webapp

---
+++ com.org.testproject2/src/main/java/HelloWorld.java
@@ -0,0 +1,16 @@
+
+
+public class HelloWorld {
+
+	public void printmethod() {
+		System.out.println("Hello World");
+	}
+	
+
+	public static void main(String[] args) {
+	// TODO Auto-generated method stub
+
+	HelloWorld helloWorld = new HelloWorld();
+	helloWorld.printmethod();
+}
+}
